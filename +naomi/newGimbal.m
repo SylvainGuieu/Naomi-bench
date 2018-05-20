56ed4c3105df234869abac0e7417d256b51bb3ce
Sylvain Guieu <sylvain.guieu@gmail.com>
2018-05-20 09:03:01 +0200

    dev

--- +naomi/newGimbal.m
+++ +naomi/newGimbal.m
@@ -1,17 +1,10 @@
 function gimbal = newGimbal(config)
     % Starting communication with the motors of DM mount 
     % return it as a Gimbal object
-    config.log('Starting CO mount gimbal motors ...',1)
-    switch config.getLocation()
-        case {config.IPAG, config.BENCH} 
-            addpath('C:\Users\Public\PI\PI_MATLAB_Driver_GCS2');
-            gimbal = naomi.objects.Gimbal('0175500223');
-            
-        case config.ESOHQ
-            %% ADD something to do with motors at ESO-HQ ?
-            config.log('Error Gimbal motor communication is not established at ESO-HQ');
-            return ;
-    end
+    config.log('Starting CO mount gimbal motors ...',1);
+    addpath(config.piMotorDriver);
+    gimbal = naomi.objects.Gimbal(config.piControlerSerial);
+    
     gimbal.rX.zero = config.gimbalRxZero;
     gimbal.rY.zero = config.gimbalRyZero;
     gimbal.rX.gain = config.gimbalRxGain;
